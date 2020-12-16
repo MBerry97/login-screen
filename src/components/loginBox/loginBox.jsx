19910c01eb19a86c5d690c6345982fb675070024
MBerry97 <imatty2011@hotmail.co.uk>
2020-12-16 20:05:36 +0000

    styling to login button

--- src/components/loginBox/loginBox.jsx
+++ src/components/loginBox/loginBox.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './loginBox.css'
-import LoginButton from './loginButton/loginButton';
 import LoginInputForm from './loginInputForm/loginInputForm';
 import LoginText from './loginText/loginText';
 import RememberPassword from './rememberPassword/rememberPassword';
@@ -22,10 +21,8 @@ const LoginBox = (props) => {
   return (
     <div className='loginBox__container'>
       <div className='loginBox__centralContainer'>
-        
       <LoginText  text1={props.text1} text2={props.text2}/>
       <LoginInputForm  onLoginDispatch={props.onLogin}/>
-      
       <RememberPassword  />
       <JumpingDotsAnim />
       <LoadingAnim />
