9ab811c0452f25f0597f3486a8467708bfda4613
MBerry97 <imatty2011@hotmail.co.uk>
2020-12-17 17:50:07 +0000

    removed border from button

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@
 import './App.css';
 import LoginImage from './components/loginImage/loginImage';
 import LoginBox from './components/loginBox/loginBox';
-import {Spring} from 'react-spring/renderprops'
 
 const App = () => {
   return (
     <div className="App">
       <LoginImage />
-      
       <LoginBox />
-      
-      
     </div>
   );
 }
